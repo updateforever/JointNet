de45026834575d6c22d84fe481fcab5cfbee23da
“updateforever” <1456742985@qq.com>
2023-05-26 17:59:27 +0800

    modify

--- setting/pred_setting/seg.py
+++ setting/pred_setting/seg.py
@@ -1,3 +1,5 @@
+from unittest import loader
+
 from tqdm import tqdm
 import network
 import utils
@@ -9,6 +11,8 @@ import numpy as np
 from torch.utils import data
 from datasets import VOCSegmentation, Cityscapes, cityscapes, house2k_seg
 from torchvision import transforms as T
+
+from datasets.house_seg_2k import voc_cmap
 from metrics import StreamSegMetrics
 
 import torch
@@ -32,7 +36,7 @@ def main(opts):
         image_files.append(opts.input)
 
     # Set up model (all models are 'constructed at network.modeling)
-    model = network.modeling.__dict__[opts.model](num_classes=opts.num_classes, output_stride=opts.output_stride)
+    model = network.modeling.__dict__[opts.model](num_classes=opts.num_classes + 2, output_stride=opts.output_stride)
     if opts.separable_conv and 'plus' in opts.model:
         network.convert_to_separable_conv(model.classifier)
     utils.set_bn_momentum(model.backbone, momentum=0.01)
@@ -52,17 +56,17 @@ def main(opts):
 
     # denorm = utils.Denormalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])  # denormalization for ori images
 
-    if opts.crop_val:
+    if 1:
         transform = T.Compose([
             T.Resize(opts.crop_size),  # T.CenterCrop(opts.crop_size),
             T.ToTensor(),
             T.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
         ])
-    else:
-        transform = T.Compose([
-            T.ToTensor(),
-            T.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
-        ])
+    # else:
+    #     transform = T.Compose([
+    #         T.ToTensor(),
+    #         T.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+    #     ])
     if opts.save_val_results_to is not None:
         os.makedirs(opts.save_val_results_to, exist_ok=True)
     with torch.no_grad():
@@ -77,5 +81,18 @@ def main(opts):
             pred = model(img).max(1)[1].cpu().numpy()[0]  # HW
             colorized_preds = opts.decode_fn(pred).astype('uint8')
             colorized_preds = Image.fromarray(colorized_preds)
+
+            # if 0:
+            #     pred1 = voc_cmap(pred).astype(np.uint8)
+            #     fig = plt.figure()
+            #     plt.imshow(img)
+            #     plt.axis('off')
+            #     plt.imshow(pred1, alpha=0.7)
+            #     ax = plt.gca()
+            #     ax.xaxis.set_major_locator(matplotlib.ticker.NullLocator())
+            #     ax.yaxis.set_major_locator(matplotlib.ticker.NullLocator())
+            #     plt.savefig('results/%d_overlay.png' % img_name, bbox_inches='tight', pad_inches=0)
+            #     plt.close()
+
             if opts.save_val_results_to:
                 colorized_preds.save(os.path.join(opts.save_val_results_to, img_name + '.png'))
